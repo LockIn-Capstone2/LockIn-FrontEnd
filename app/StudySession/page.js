de2229d65c27cf0a46c84cbcad01f16d0a5a8412
pedrosortega <psortega2004@gmail.com>
2025-08-01 14:46:31 -0400

    added basic ui for study timer

--- app/StudySession/page.js
+++ app/StudySession/page.js
@@ -1,3 +1,4 @@
+"use client"; //used for client side interactivity
 import "./style.module.css";
 import {
   Card,
@@ -8,19 +9,22 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import React from "react";
 import Link from "next/link";
+
 function StudyTimer() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
     <>
       <div className="flex font-[poppins] bg-[#2D3142] text-[white] text-center rounded-xl h-8 items-center justify-center font-bold">
         Choose a Time
       </div>
       <div className="flex items-center justify-center min-h-screen">
-        <Card className="flex w-full max-w-sm bg-[#2D3142] border-none">
+        <Card className="flex w-full max-w-sm bg-[#2D3142] border-none h-100">
           <CardHeader>
             <CardTitle className="font-[poppins] text-[white] text-center">
               Study Timer
@@ -28,8 +32,36 @@ function StudyTimer() {
             <CardDescription></CardDescription>
             <CardAction></CardAction>
           </CardHeader>
-          <CardContent></CardContent>
-          <CardFooter className="flex-col gap-2"></CardFooter>
+          <CardContent className=" flex w-40 h-40 rounded-full aspect-square items-center justify-center ring-4 mx-auto ring-[#EEC0C8]"></CardContent>
+          <CardFooter className="flex-col gap-2">
+            <button
+              onClick={handleClick}
+              type="submit"
+              className="rounded-lg bg-[#0D1321] text-[white] top-[5] right-[50] font-bold font-[poppins] text-center relative w-[65]"
+            >
+              START
+            </button>
+            <button
+              onClick={handleClick}
+              type="submit"
+              className="rounded-lg bg-[#0D1321] text-[white] bottom-[27] left-[50] font-bold font-[poppins] text-center relative w-[65]"
+            >
+              STOP
+            </button>
+            <div className="flex wrap space-x-5">
+              <Input
+                type="number"
+                placeholder="Hours"
+                className={"w-25 "}
+              ></Input>
+              <span className="text-white font-[poppins]">:</span>
+              <Input
+                type="number"
+                placeholder="Minutes"
+                className={"w-28"}
+              ></Input>
+            </div>
+          </CardFooter>
         </Card>
       </div>
     </>
