141816ff1acfa10063059b270974bd84ba887263
Elian Echavarria <elianstanly431@gmail.com>
2025-08-13 14:10:47 -0400

    Refactor the Not Found Component
    Redesigned the 'Not Found' component for improved visual appearance and user experience.

--- app/not-found.js
+++ app/not-found.js
@@ -5,6 +5,7 @@ import { Rubik_Glitch_Pop } from "next/font/google";
 import { gsap } from "gsap";
 import "./globals.css";
 import Link from "next/link";
+import FuzzyText from "@/components/ui/FuzzyText";
 
 const rubikGlitch = Rubik_Glitch_Pop({
   subsets: ["latin"],
@@ -14,26 +15,13 @@ const rubikGlitch = Rubik_Glitch_Pop({
 const NotFound = () => {
   useEffect(() => {
     gsap.fromTo(
-      ".error-title",
-      { opacity: 0, y: -100, scale: 0.5, rotate: -15 },
-      {
-        opacity: 1,
-        y: 0,
-        scale: 1,
-        rotate: 0,
-        duration: 1.2,
-        ease: "elastic.out(1, 0.5)",
-      }
-    );
-
-    gsap.fromTo(
       ".error-subtitle",
       { opacity: 0, y: 50 },
       {
         opacity: 1,
         y: 0,
         duration: 1,
-        delay: 0.8,
+        delay: 1.2,
         ease: "power2.out",
         onComplete: () => {
           gsap.to(".error-subtitle", {
@@ -47,27 +35,6 @@ const NotFound = () => {
       }
     );
 
-    gsap.to(".error-title", {
-      keyframes: [
-        { x: -2, duration: 0.02 },
-        { x: 2, duration: 0.02 },
-        { x: -1, duration: 0.02 },
-        { x: 1, duration: 0.02 },
-        { x: 0, duration: 0.02 },
-      ],
-      repeat: -1,
-      repeatDelay: 3,
-      delay: 1.5,
-    });
-
-    gsap.to(".error-title", {
-      y: "+=40",
-      duration: 2,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut",
-    });
-
     gsap.to(".error-subtitle", {
       x: "+=5",
       duration: 3,
@@ -77,21 +44,30 @@ const NotFound = () => {
     });
   }, []);
 
+  const hoverIntensity = 0.5;
+  const enableHover = 0.5;
+
   return (
     <main
       suppressHydrationWarning
       className="relative h-screen flex flex-col items-center justify-center bg-black"
     >
-      <h1
-        className={`${rubikGlitch.className} text-9xl error-title text-white`}
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={hoverIntensity}
+        enableHover={enableHover}
+        className="error-title-main"
       >
         404
-      </h1>
-      <span
-        className={`${rubikGlitch.className} text-xl mt-4 error-subtitle text-white`}
+      </FuzzyText>
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={hoverIntensity}
+        enableHover={enableHover}
+        className="error-title-secondary"
       >
         Are you lost?
-      </span>
+      </FuzzyText>
       <Link
         href="/"
         suppressHydrationWarning
