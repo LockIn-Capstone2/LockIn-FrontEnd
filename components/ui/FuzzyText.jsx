f2c45db142598a53d11ded9cdc8dd171c2d155a0
Elian Echavarria <elianstanly431@gmail.com>
2025-08-14 11:24:11 -0400

    Font Size Change for the 404 component
    Increase the 404 text size to improve readability.

--- components/ui/FuzzyText.jsx
+++ components/ui/FuzzyText.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 
 const FuzzyText = ({
   children,
-  fontSize = "clamp(2rem, 10vw, 5rem)",
+  fontSize = "clamp(2rem, 10vw, 10rem)",
   fontWeight = 900,
   fontFamily = "inherit",
   color = "#fff",
