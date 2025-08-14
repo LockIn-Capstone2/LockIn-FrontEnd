197b4cedfe915058f42ff8b609a93c19ea343761
Benjamin Ayala <Benjaminayala607@gmail.com>
2025-08-14 11:17:16 -0400

    merged

--- app/StudySession/page.js
+++ app/StudySession/page.js
@@ -134,7 +134,7 @@ function StudyTimer() {
             </button>
             <div className="flex wrap space-x-5">
               <Input
-                type="number"
+                type="text"
                 placeholder="HH"
                 className={"w-13 text-center"}
                 onChange={handleHoursChange}
